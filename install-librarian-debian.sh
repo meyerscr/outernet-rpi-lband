d412556034cbff667537edb3e88837c56a7e2abd
Branko Vukelic <branko@outernet.is>
2016-09-22 09:59:58 +0200

    Various fixes
    - Bail on errors
    - Do not patch distlib
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- install-librarian-debian.sh
+++ install-librarian-debian.sh
@@ -12,36 +12,42 @@ DLDIR="${DLDIR:=/srv/downloads}"
 
 export DEBIAN_FRONTEND
 
+fail() {
+  msg="$1"
+  echo "$msg"
+  exit 1
+}
+
 echo "Adding testing repositories"
 cat /etc/apt/sources.list | sed 's/jessie/testing/' \
   > /etc/apt/sources.list.d/testing.list
 
 echo "Install dependencies"
+$APT update || fail "Could not update local package database"
 $APT install python python-pip postgresql libev-dev libpq-dev python-dev \
-	build-essential
+	build-essential || fail "Could not install dependencies"
 
-echo "Fix distlib"
-wget "$POOL_URL/d/distlib/python-distlib_0.1.9-1_all.deb"
-wget "$POOL_URL/d/distlib/python-distlib-whl_0.1.9-1_all.deb"
-dpkg -i *.deb
-rm *.deb
+python=$(which python2)
+site_packages=$(${python} -c 'import site; print(site.getsitepackages()[0])')
 
 echo "Configuring PostgreSQL"
-sed -ie 's|^(host .*) md5$|\1 trust|' "$PGHBA"
-service postgresql restart
+sed -ie 's|^\(host .*\) md5$|\1 trust|' "$PGHBA" \
+  || fail "Could not configure PostgreSQL"
+service postgresql restart || fail "Could not start PostgreSQL"
 
 echo "Installing Librarian"
 pip install \
   --extra-index-url "https://archive.outernet.is/sources/pypi/simple/" \
-  "https://github.com/Outernet-Project/librarian/archive/$LIBRARIAN_VER.tar.gz"
+  "https://github.com/Outernet-Project/librarian/archive/$LIBRARIAN_VER.tar.gz" \
+  || fail "Could not install Librarian"
 
 echo "Configuring Librarian and FSAL"
 mkdir -p /var/librarian
 chmod 777 /var/librarian
 install -Dm644 "${ANY_SRC}/librarian.ini" /etc/librarian.ini
 install -Dm644 "${ANY_SRC}/fsal.ini" /etc/fsal.ini
-sed_xp="s|%SITE_PACKAGES%|$SITE_PACKAGES|g;s|%PLATFORM%|$DISTRO|g"
-sed -ie "$sed_xp" /etc/librarian.ini
-sed -ie "$sed_xp" /etc/fsal.ini
-python=$(which python2)
-site_packages=$(${PYTHON} -c 'import site; print(site.getsitepackages()[0])')
+sed_xp="s|%SITE_PACKAGES%|$site_packages|g;s|%PLATFORM%|$DISTRO|g"
+sed -ie "$sed_xp" /etc/librarian.ini || fail "Could not configure Librarian"
+sed -ie "$sed_xp" /etc/fsal.ini || fail "Could not configure FSAL"
+
+echo "Finished installing the web interface"
